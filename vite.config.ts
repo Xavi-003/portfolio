24549b6310976c4e158714abe84f7e98a026c1c7
Antony Xavier <xavier.developer03@gmail.com>
2025-11-26 16:01:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ mode }) => {
       }
     },
     define: {
-      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY),
     }
   };
 });
